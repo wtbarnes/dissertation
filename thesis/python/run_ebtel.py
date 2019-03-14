a6a0809de8032bba39ebadff77d9d082961cf095
Will Barnes <wtb2@rice.edu>
2019-03-13 19:19:19 -0500

    first full pass at loops chapter

--- thesis/python/run_ebtel.py
+++ thesis/python/run_ebtel.py
@@ -9,14 +9,22 @@ import numpy as np
 import astropy.units as u
 from synthesizAR.interfaces.ebtel import write_xml
 
+
 def run_ebtel(config, ebtel_dir):
     with tempfile.TemporaryDirectory() as tmpdir:
         config['output_filename'] = os.path.join(tmpdir, 'ebtelplusplus.tmp')
-        write_xml(config,os.path.join(tmpdir,'ebtelplusplus.tmp.xml'))
+        write_xml(config, os.path.join(tmpdir, 'ebtelplusplus.tmp.xml'))
         subprocess.call([os.path.join(ebtel_dir, 'bin', 'ebtel++.run'),
-                         '-c', os.path.join(tmpdir,'ebtelplusplus.tmp.xml')])
+                         '-c', os.path.join(tmpdir, 'ebtelplusplus.tmp.xml')])
         data = np.loadtxt(config['output_filename'])
-    return {
+        terms = {}
+        if config['save_terms']:
+            data_terms = np.loadtxt(config['output_filename']+'.terms')
+            terms['f_e'] = data_terms[:,0]
+            terms['f_i'] = data_terms[:,1]
+            terms['c_1'] = data_terms[:,2]
+            terms['rad'] = data_terms[:,3]
+    return {**{
         'time': data[:, 0]*u.s,
         'electron_temperature': data[:, 1]*u.K,
         'ion_temperature': data[:, 2]*u.K,
@@ -24,4 +32,4 @@ def run_ebtel(config, ebtel_dir):
         'electron_pressure': data[:, 4]*u.dyne/(u.cm**2),
         'ion_pressure': data[:, 5]*u.dyne/(u.cm**2), 
         'velocity': data[:, 6]*u.cm/u.s,
-    }
+    }, **terms}
